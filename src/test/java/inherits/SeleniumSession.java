e749a408cbc551e11a3ccb96970ad8b903f7e42a
gwyckaert <geoffroy.wyckaert@dgfip.finances.gouv.fr>
2020-06-04 22:12:48 +0200

    Correctif test Juice et incoherence d'initialisation de l'objet de test maj props nubo

--- src/test/java/inherits/SeleniumSession.java
+++ src/test/java/inherits/SeleniumSession.java
@@ -14,47 +14,78 @@ import org.openqa.selenium.support.events.EventFiringWebDriver;
 import com.google.common.flogger.FluentLogger;
 import com.google.common.flogger.StackSize;
 
-public class SeleniumSession extends DataContext {
-	private static final FluentLogger LOGGER = FluentLogger.forEnclosingClass();
+public class SeleniumSession extends DataContext
+{
+    private static final FluentLogger LOGGER = FluentLogger.forEnclosingClass();
 
-	protected static boolean activateZap;
+    protected static boolean activateZap;
 
-	protected final static String FIREFOX = "firefox";
+    protected final static String FIREFOX = "firefox";
 
-	protected final static String CHROME = "chromium";
+    protected final static String CHROME = "chromium";
 
-	public static final boolean IS_HEADLESS_DEFAULT = true;
+    public static final boolean IS_HEADLESS_DEFAULT = true;
 
-	protected String browser;
+    protected String browser;
 
-	protected boolean isFirefox;
+    protected boolean isFirefox;
 
-	protected MutableCapabilities options;
+    protected MutableCapabilities options;
 
-	protected WebDriver webDriver;
+    protected WebDriver webDriver;
 
-	protected EventFiringWebDriver driver;
+    protected EventFiringWebDriver driver;
 
-	public SeleniumSession(String browser) {
-		isFirefox = FIREFOX.equalsIgnoreCase(browser);
-		if (isFirefox) {
-			options = new FirefoxOptions().setHeadless(IS_HEADLESS_DEFAULT);
-			if (activateZap) {
-				((FirefoxOptions) options).setProxy(new Proxy().setHttpProxy(adressZap + ":" + portZap));
-			}
+    public SeleniumSession(String browser)
+    {
+        isFirefox = FIREFOX.equalsIgnoreCase(browser);
+        if (isFirefox)
+        {
+            options = new FirefoxOptions().setHeadless(IS_HEADLESS_DEFAULT);
+            if (activateZap)
+            {
+                ((FirefoxOptions) options).setProxy(new Proxy().setHttpProxy(adressZap + ":" + portZap));
+            }
 
-		} else {
-			options = new ChromeOptions().setHeadless(IS_HEADLESS_DEFAULT);
-		}
-		try {
-			webDriver = new RemoteWebDriver(new URL("http://" + hostGrid + ":" + portGrid + "/wd/hub"), options);
-		} catch (MalformedURLException e) {
-			LOGGER.atWarning().withStackTrace(StackSize.FULL).log("URL du grid sollicite: host %s port %s", hostGrid,
-					portGrid);
-		}
-		ListenerSelenium listener = new ListenerSelenium();
-		driver = new EventFiringWebDriver(webDriver);
-		driver.register(listener);
-	}
+        }
+        else
+        {
+            options = new ChromeOptions().setHeadless(IS_HEADLESS_DEFAULT);
+        }
+        try
+        {
+            webDriver = new RemoteWebDriver(new URL("http://" + hostGrid + ":" + portGrid + "/wd/hub"), options);
+        }
+        catch (MalformedURLException e)
+        {
+            LOGGER.atWarning().withStackTrace(StackSize.FULL).log("URL du grid sollicite: host %s port %s", hostGrid,
+                portGrid);
+        }
+        ListenerSelenium listener = new ListenerSelenium();
+        driver = new EventFiringWebDriver(webDriver);
+        driver.register(listener);
+    }
+
+    public SeleniumSession()
+    {
+        options = new FirefoxOptions().setHeadless(IS_HEADLESS_DEFAULT);
+        if (activateZap)
+        {
+            ((FirefoxOptions) options).setProxy(new Proxy().setHttpProxy(adressZap + ":" + portZap));
+        }
+
+        try
+        {
+            webDriver = new RemoteWebDriver(new URL("http://" + hostGrid + ":" + portGrid + "/wd/hub"), options);
+        }
+        catch (MalformedURLException e)
+        {
+            LOGGER.atWarning().withStackTrace(StackSize.FULL).log("URL du grid sollicite: host %s port %s", hostGrid,
+                portGrid);
+        }
+        ListenerSelenium listener = new ListenerSelenium();
+        driver = new EventFiringWebDriver(webDriver);
+        driver.register(listener);
+    }
 
 }
